2e0cb4da0e7b238be78d5a027aa386d3d3b22625
Mike Bucks <mbucks@leftfieldlabs.com>
2017-05-08 15:25:18 -0700

    adds brand component and some animation to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+
+  animations: [
+    trigger('navState', [
+      state('inactive', style({
+        opacity: .5,
+        transform: 'scale(0)'
+      })),
+      state('active',   style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      transition('inactive => active', animate('250ms ease-in')),
+      transition('active => inactive', animate('250ms ease-out'))
+    ])
+  ]
 })
+
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any[]>;
+  navPressScale: String;
+  navPosition;
+  navigationState: String;
+  navigationTimerState: String;
+
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/items');
+    this.navPressScale = '0px';
+    this.navigationState = 'inactive';
+    this.navigationTimerState = 'inactive';
+    this.navPosition = {
+      x: '0px',
+      y: '0px'
+    }
+  }
+
+  private positionNav = (evt) => {
+    this.navPosition.x = evt.clientX + 'px';
+    this.navPosition.y = evt.clientY + 'px';
+  }
+
+  private startNavTrigger = (evt) => {
+    this.navigationState = 'active';
+    window.setTimeout(() => {
+      this.navigationTimerState = 'active';
+    }, 300);
+  }
+
+  private stopNavTrigger = (evt) => {
+    this.navigationState = 'inactive';
+    this.navigationTimerState = 'inactive';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { BrandComponent } from './brand/brand.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrbPf94Qhd28VtnJu2mJTFiyw36RGA3jg",
+  authDomain: "magnificentfuture-a4d0c.firebaseapp.com",
+  databaseURL: "https://magnificentfuture-a4d0c.firebaseio.com",
+  projectId: "magnificentfuture-a4d0c",
+  storageBucket: "magnificentfuture-a4d0c.appspot.com",
+  messagingSenderId: "216192365258"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
